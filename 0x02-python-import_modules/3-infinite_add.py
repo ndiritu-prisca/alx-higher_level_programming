ad9dc25288c120634287c0e903d2f7769d8440e1
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-06 13:17:49 -0800

    Infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    for n in range(len(sys.argv) - 1):
+        sum += int(sys.argv[n + 1])
+    print("{:d}".format(sum))
