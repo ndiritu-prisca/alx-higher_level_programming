444d661deac7df3bc5a17dee3c76a519195d80a6
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-09 06:59:07 -0800

    Alternative

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in x:
-            print("{:d}". format(y), end="")
-            if y != x[-1]:
-                print(" ", end="")
+            print("{:d}".format(y), end=" " if y != x[-1] else "")
         print("")
