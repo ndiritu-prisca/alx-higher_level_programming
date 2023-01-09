8194c3583776a4841d5031ec9d439c4a865ce361
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-09 11:13:00 -0800

    Updates

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,6 @@
 #include <Python.h>
-#include <stdio.h>
+#include <listobject.h>
+#include <object.h>
 
 /**
   * print_python_list_info - a function that prints information about
@@ -9,5 +10,13 @@
   */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t listSize = Pylist_Size();
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i,
+				Py_TYPE(obj->ob_item[i])->tp_name);
 }
