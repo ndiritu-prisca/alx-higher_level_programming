a4feb2df2c1531bf2000b208cc8d12f273b92cb4
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-09 11:09:27 -0800

    Include description

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,12 @@
 #include <Python.h>
 #include <stdio.h>
 
+/**
+  * print_python_list_info - a function that prints information about
+  * a python list object
+  * @p: pointer to generic PyObject which is of PyListObject type
+  * Return: nothing
+  */
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t listSize = Pylist_Size();
