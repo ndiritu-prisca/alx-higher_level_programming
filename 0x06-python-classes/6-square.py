9b8fe0a2e3d13cc107c9e51b8e4aa48b7d16dfa3
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-24 12:26:50 -0800

    Use if

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,17 +34,16 @@ class Square:
         if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
             self.__position = None
-        elif len(value) != 2:
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
             self.__position = None
-        elif type(value[0]) is not int or type(value[1]) is not int:
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
             self.__position = None
-        elif value[0] < 0 or value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
             self.__position = None
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """Returns the area of a square"""
