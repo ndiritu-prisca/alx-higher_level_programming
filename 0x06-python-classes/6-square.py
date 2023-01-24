e301a1f26cf87ce4c362a0d132b7244041f0130c
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-24 12:39:10 -0800

    Another trial

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,9 +50,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            [print() for i in range(self.__position[1])]
+            for i in range(self.__position[1]):
+                print()
             for x in range(self.__size):
-                [print(" ", end="") for j in range(self.__position[0])]
+                for j in range(self.__position[0]):
+                    print(" ", end="")
                 for y in range(self.__size):
                     print("#", end="")
                 print()
