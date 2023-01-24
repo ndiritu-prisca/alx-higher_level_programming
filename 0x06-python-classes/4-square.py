44111d6a1bc7d18ff7e4e0e7ddd49aa7fc03a8e8
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-24 08:17:21 -0800

    Setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a new square with a private instance attribute"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of square if value is an int and is greater that 0"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of a square"""
+        return (self.__size ** 2)
