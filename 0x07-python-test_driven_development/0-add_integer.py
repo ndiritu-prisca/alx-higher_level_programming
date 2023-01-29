344f10726c249bfa430cc668a0188787d3ada116
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-29 12:59:31 +0000

    Add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    An addition module.
+    It has an addition function that takes in integers or floats as arguments. 
+    The float values are typecatsed into ints and the sum of 2 ints is returned..
+"""
+
+
+def add_integer(a, b=98):
+    """
+        A function that adds 2 integers
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
