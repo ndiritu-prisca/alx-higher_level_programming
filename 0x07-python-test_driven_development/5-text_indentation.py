c88b75058a899068b9054309793ef7f1da0567dc
ndiritu-prisca <priscandiritu@outlook.com>
2023-01-30 08:43:54 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,13 +10,17 @@ def text_indentation(text):
     """
         A function that indents a string passed as text and raises any error.
     """
-    print("{}".format(len(text)))
+    flag = 0
     if type(text) is not str:
         raise TypeError("text must be a string")
     for char in text:
-        if char == '.' or char == '?' or char == ':':
+        if char == ' ' and flag == 1:
+            pass
+        elif char == '.' or char == '?' or char == ':':
             print("{}".format(char), end="")
             print()
             print()
+            flag = 1
         else:
             print("{}".format(char), end="")
+            flag = 0
