afb73de8bd51885ae96674e1311841fc5915051f
ndiritu-prisca <priscandiritu@outlook.com>
2023-02-01 15:39:00 +0000

    Correct output for 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print()
+        pass
     for i in range(size):
         for j in range(size):
             print("#", end="")
