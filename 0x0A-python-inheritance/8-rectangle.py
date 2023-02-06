8f31b23832b72e2ea383d256f84efa1a66f6218b
ndiritu-prisca <priscandiritu@outlook.com>
2023-02-06 18:57:54 +0000

    Subclass

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defining a module"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """A method with private attributes: width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
