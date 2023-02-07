5e1b4e0ff3b3784a8dc02cafd340484759a1fd95
ndiritu-prisca <priscandiritu@outlook.com>
2023-02-07 21:33:07 +0000

    Updates

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defining a module"""
+
+
+class Student:
+    """A class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            A public method that retrieves a dictionary representation of a
+            Student instance
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict_
+
+    def reload_from_json(self, json):
+        """
+            A public method that replaces all attributes of the Student
+            instance
+        """
+        for ele in json:
+            self.__dict__[ele] = json[ele]
