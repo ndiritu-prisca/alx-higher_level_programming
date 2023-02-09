991ed2cc88679f6740473ffc688d69367ca0533b
ndiritu-prisca <priscandiritu@outlook.com>
2023-02-09 09:35:43 +0000

    Append after specific string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defining a function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+        A function that inserts a line of text to a file, after each line
+        containing a specific string
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
