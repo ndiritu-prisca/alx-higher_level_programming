ad362500cd04d639c73708b3a803e5cb8971eb25
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-15 18:05:27 +0000

    Permissions

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates the MySQL server user user_0d_1 having all privileges
+-- user_0d_1 password should be set to user_0d_1_pwd
+-- script should not fail if user exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
