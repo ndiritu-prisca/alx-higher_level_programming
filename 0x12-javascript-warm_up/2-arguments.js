605ff5f9d1da30850401caa4f3da9ab6dc1cebb5
ndiritu-prisca <priscandiritu@outlook.com>
2023-04-11 22:13:43 +0000

    Use all args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // A script that prints a message depending of the number of arguments passed
 
-const args = process.argv.slice(2);
+const args = process.argv;
 const numArgs = args.length;
-if (numArgs === 0) {
+if (numArgs === 2) {
   console.log('No argument');
-} else if (numArgs === 1) {
+} else if (numArgs === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
