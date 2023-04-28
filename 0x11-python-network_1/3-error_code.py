6c27e393c0942d39017eb8cf97f3154fa5b755dd
ndiritu-prisca <priscandiritu@outlook.com>
2023-04-28 21:13:16 +0000

    Remove trailing whitespace

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,7 +3,7 @@
     A script that takes in a URL, sends a request to the URL and displays the
     body of the response (decoded in utf-8).
 """
-from urllib import request, parse, error
+from urllib import request, error
 import sys
 
 
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code:", e.code) 
+    except HTTPError as e:
+        print("Error code:", e.code)
