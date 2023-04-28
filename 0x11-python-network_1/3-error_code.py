6e62e665178a0739eabc0fc271e00fe349384239
ndiritu-prisca <priscandiritu@outlook.com>
2023-04-28 21:10:55 +0000

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+"""
+    A script that takes in a URL, sends a request to the URL and displays the
+    body of the response (decoded in utf-8).
+"""
+from urllib import request, parse, error
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code:", e.code) 
