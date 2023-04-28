b75e8492d50123b7cfdd9f4544eb49811a9f60ce
ndiritu-prisca <priscandiritu@outlook.com>
2023-04-28 21:01:12 +0000

    Add :

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,11 +8,11 @@ from urllib import request, parse
 import sys
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with request.urlopen(request) as response:
-        print(response.read().decode("utf-8")) 
+        print(response.read().decode("utf-8"))
